e0b9603a69386f21c67fa647e0ba3fb03e546925
lovepreet682 <94538091+lovepreet682@users.noreply.github.com>
2023-11-24 18:06:32 +0530

    Delete Redux directory

--- Redux/redux-toolkit/src/components/TodoList.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function TodoList() {
-  return (
-    <>TodoList</>
-  )
-}
-
-export default TodoList
--- Redux/redux-toolkit/src/myStore/Store.js
+++
@@ -1,6 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { todoList } from "../features/myReducers/TodoSlice";
-
-export const store = configureStore({
-    reducer:todoList
-})
